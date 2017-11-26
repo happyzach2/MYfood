dbde123a3bfcdb653a01ceca0169a6cd86007489
zhary <schneider61938@gmail.com>
2017-11-25 23:35:45 -0600

    add button to home page meals

--- app/controllers/week_day_meals_controller.rb
+++ app/controllers/week_day_meals_controller.rb
@@ -4,7 +4,7 @@ class WeekDayMealsController < ApplicationController
     @week_day = WeekDay.find(params[:week_day_id])
     @meal = UserMeal.find(params[:meal_id]).meal
     @week_day_meal = WeekDayMeal.create!(week_day_id: @week_day.id , meal_id: @meal.id)
-    redirect_to @week_day, notice: "Meal added to day"
+    redirect_to pages_home_path, notice: "Meal added to day"
   end
 
   def edit
